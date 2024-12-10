f198fbcf21091004eb9a73e18d3918956c3c6c46
markeddolphin <76056928+markeddolphin@users.noreply.github.com>
2024-12-09 21:08:59 -0500

    update interface

---
+++ main.cjs
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Load React app
+  mainWindow.loadURL('http://localhost:3010'); // For development mode
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
